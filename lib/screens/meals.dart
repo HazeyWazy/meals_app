651d620d2275750af5f6d9166b3b3cda1db79db5
Htet Yamin Ei 47743220 <htetyamin.ei@students.mq.edu.au>
2025-05-14 16:38:14 +1000

    Add meals data

---
+++ lib/screens/meals.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class MealsScreen extends StatelessWidget {
+  const MealsScreen({
+    super.key,
+    required this.title,
+    required this.meals,
+  });
+
+  final String title;
+  final List<String> meals;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: const Center(
+        child: Text('The meals screen'),
+      ),
+    );
+  }
+}
