7dd2bd8dbac4ab0a9bcacae8cd59204568ed4657
Htet Yamin Ei 47743220 <htetyamin.ei@students.mq.edu.au>
2025-05-14 16:55:04 +1000

    Load meals data into a screen

--- lib/screens/meals.dart
+++ lib/screens/meals.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:meals_app/models/meal.dart';
 
 class MealsScreen extends StatelessWidget {
   const MealsScreen({
@@ -8,17 +9,47 @@ class MealsScreen extends StatelessWidget {
   });
 
   final String title;
-  final List<String> meals;
+  final List<Meal> meals;
 
   @override
   Widget build(BuildContext context) {
+    Widget content = ListView.builder(
+      itemCount: meals.length,
+      itemBuilder: (ctx, index) => Text(
+        meals[index].title,
+      ),
+    );
+
+    if (meals.isEmpty) {
+      content = Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text(
+              'Uh oh...nothing here!',
+              style: Theme.of(context)
+                  .textTheme
+                  .headlineLarge!
+                  .copyWith(color: Theme.of(context).colorScheme.onSurface),
+            ),
+            const SizedBox(height: 16),
+            Text(
+              'Try selecting a different category!',
+              style: Theme.of(context)
+                  .textTheme
+                  .bodyLarge!
+                  .copyWith(color: Theme.of(context).colorScheme.onSurface),
+            )
+          ],
+        ),
+      );
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: Text(title),
       ),
-      body: const Center(
-        child: Text('The meals screen'),
-      ),
+      body: content,
     );
   }
 }
